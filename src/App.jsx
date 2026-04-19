443f892e794ec5a5133353f9fe287641a5ada483
Iz <innovant_devr@outlook.com>
2026-04-19 01:12:06 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,28 @@ import {WINDOW_CONFIG} from "#constants";
 import Navbar from "#components/Navbar.jsx";
 import {Welcome} from "#components/index.js";
 import Dock from "#components/Dock.jsx";
-import {Finder, Resume, Safari, Terminal, Text, Images, Contact, Photos} from "#windows";
+import { Finder, Resume, Safari, Terminal, Text, Images, Contact } from "#windows";
 import Home from "#components/Home.jsx";
+import useWindowStore from "#store/window.js";
 
 gsap.registerPlugin(Draggable);
 
 const App = () => {
+
+    const { windows } = useWindowStore();
+    const isAnyWindowOpen = Object.values(windows).some((window) => window.isOpen);
+
     return (
         <main>
             <Navbar />
             <Welcome />
             <Dock />
+
+            <div
+                className={`absolute inset-0 z-40 bg-black/15 backdrop-blur-md transition-opacity duration-300 pointer-events-none ${
+                    isAnyWindowOpen ? "opacity-100" : "opacity-0"
+                }`}
+            />
 
             <Terminal />
             <Safari />
@@ -27,7 +38,7 @@ const App = () => {
             <Text />
             <Images />
             <Contact />
-            <Photos />
+
 
             <Home />
         </main>
--- src/windows/index.js
+++ src/windows/index.js
@@ -6,6 +6,6 @@ import Finder from "#windows/Finder.jsx";
 import Text from "#windows/Text.jsx";
 import Images from "#windows/Images.jsx";
 import Contact from "#windows/Contact.jsx";
-import Photos from "#windows/Photos.jsx";
 
-export { Terminal, Safari, Resume, Finder, Text, Images, Contact, Photos };
+
+export { Terminal, Safari, Resume, Finder, Text, Images, Contact };
